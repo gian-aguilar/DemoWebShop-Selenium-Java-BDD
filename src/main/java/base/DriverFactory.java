da128be8db1dd5581362c57ae8bda7c25ad94ff3
gian-aguilar <gkdaguilar@gmail.com>
2026-03-06 10:17:42 +0800

    Fix: Enabled headless mode and added report upload

--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -19,11 +19,10 @@ public class DriverFactory {
         if (driver == null) {
             if (browserName.equalsIgnoreCase("chrome")) {
             	ChromeOptions options = new ChromeOptions();
-            	options.addArguments("--headless=new"); 
-            	options.addArguments("--disable-gpu");
-            	options.addArguments("--window-size=1920,1080");
-            	options.addArguments("--no-sandbox");
-            	options.addArguments("--disable-dev-shm-usage");
+            	options.addArguments("--headless=new"); // Runs Chrome without a UI
+            	options.addArguments("--no-sandbox"); // Required for Linux environments
+            	options.addArguments("--disable-dev-shm-usage"); // Overcomes resource limits in containers
+            	options.addArguments("--window-size=1920,1080"); // Sets a virtual resolution
             	
             	driver = new ChromeDriver(options);
             } else if (browserName.equalsIgnoreCase("firefox")) {
